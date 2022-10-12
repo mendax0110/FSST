9f0d6d6f2b0245a98b3a5b929d52446a75908199
m3ndax <adrian.goessl@outlook.com>
2022-10-12 19:48:07 +0200

    update code and comment

--- 01_programmentwurf_programme_cs/KW40_Terminkoordination/Terminkoordination/Program.cs
+++ 01_programmentwurf_programme_cs/KW40_Terminkoordination/Terminkoordination/Program.cs
@@ -6,14 +6,17 @@ class Terminkoordination
     static void Main(string[] args)
     {
         int teilnehmerzahl;
+        // Teilnehmerzahl einlesen
         Console.WriteLine("Wie viele Teilnehmer sind zugesagt?");
         teilnehmerzahl = Convert.ToInt32(Console.ReadLine());
         
         for (int i = 0; i <= teilnehmerzahl; i++)
         {
+            // Überprüfen, ob alle Teilnehmer zufrieden sind
             Console.WriteLine("Teilnehmer {0} ist zufrieden?", i);
             string antwort = Console.ReadLine();
             
+            // wenn ja -> Termin bestätigen, wenn nein -> Termin abbrechen
             if (antwort == "ja")
             {
                 Console.WriteLine("Termin wird bestätigt");
